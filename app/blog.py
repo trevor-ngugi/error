bdc797e2e9c124a32fde254bc2734833932aab36
Trinity John <trinityrtime@gmail.com>
2020-02-12 01:07:57 +0300

    bootstrap add and div class

---
+++ app/blog.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+#
+pitchhs = [
+    {
+        'author': 'Trinity',
+        'title': 'blog post 1',
+        'content': 'first post contant',
+        'date_posted': 'feb 14, 2019'
+    },
+        {
+        'author': 'Race',
+        'title': 'blog post 2',
+        'content': 'second post contant',
+        'date_posted': 'feb 16, 2019'
+    }
+    ]
+
+@app.route('/')
+@app.route("/home")
+def home():
+    return render_template('index.html',pitchs=pitchhs)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title=about)
+if __name__ == '__main__':
+    app.run(debug=True)
