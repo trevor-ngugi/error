628b6c7a0f1624cf68dc1b12bcc8614bd030378e
Trinity John <trinityrtime@gmail.com>
2020-02-12 03:35:43 +0300

    error at form import

--- app/blog.py
+++ app/blog.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template, url_for
+from app.forms import RegisterForm, LoginForm
 app = Flask(__name__)
-#
+
+app.config['SECRET_KEY']= 'mine101'
+
 pitchhs = [
     {
         'author': 'Trinity',
@@ -24,5 +27,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title=about)
+
+@app.route("/register")
+def register():
+    form = RegisterForm()
+    return render_template('register.html', title=Register,forms=forms)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title=Login,forms=forms)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
