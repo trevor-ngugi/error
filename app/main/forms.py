b2c832ced9a89aea83a5a7b7a2db993f1f196f39
Trinity John <trinityrtime@gmail.com>
2020-02-19 09:00:22 +0300

    forms classes update

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,6 +4,18 @@ from wtforms import StringField ,PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
+class PitchForm(FlaskForm):
+    content = TextAreaField('INPUT PITCH')
+    submit = SubmitField('SUBMIT')
+
+class CommentForm(FlaskForm):
+    opinion = TextAreaField('WRITE COMMENT')
+    submit = SubmitField('SUBMIT')
+
+class CategoryForm(FlaskForm):
+    name =  StringField('Category Name', validators=[Required()])
+    submit = SubmitField('Create')
+
 class RegisterForm(FlaskForm):
   username= StringField('Username', validators=[DataRequired(), Length(min=2, max=10) ])
   email= StringField('Email', validators=[DataRequired(), Email()])
