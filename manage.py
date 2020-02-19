034e2b4ba710dcace508d939524ebc1766893e64
Trinity John <trinityrtime@gmail.com>
2020-02-19 10:45:42 +0300

    manage.py file edit

--- manage.py
+++ manage.py
@@ -1,35 +1,31 @@
-import os
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User, Pitch, Comments, PitchCategory
+#from flask_migrate import Migrate, MigrateCommand
 
-class Config:
-  # url
+# create app instance
+app = create_app('development')
 
-class ProdConfig(Config):
-  pass
+manager = Manager(app)
+manager.add_command('server', Server)
 
-class DevConfig(Config):
-  DEBUG= True
+# migrate = Migrate(app, db)
+# manager.add_command('db', MigrateCommand)
 
-config_options = {
-  'development':DevConfig,
-  'production':ProdConfig
-}
-
-
-
-
-
-
+@manager.command
+def test():
+    """
+    Run unit tests.
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-# # from app import create_app
-# # from flask_script import Manager, Server
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Pitch = Pitch, Comments=Comments, PitchCategory=PitchCategory)
+    # pass
 
-# # create app instance
-# app = create_app('development')
-
-# manager = Manager(app)
-# manager.add_command('server', Server)
-
-# #manager.add_command('server', Server)
-# if __name__ == '__main__':
-#     manager.run()
+if __name__ == '__main__':
+    manager.run()
