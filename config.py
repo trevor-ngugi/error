92aeb82b890ef8ba96af1548d1765dc022d94904
Trinity John <trinityrtime@gmail.com>
2020-02-19 09:22:44 +0300

    config.py modified and commented out pass

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 import os
 class Config:
-  pass
+  SECRET_KEY = os.environ.get("SECRET_KEY")
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  UPLOADED_PHOTOS_DEST = 'app/static/photos'
+  # #DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+  ## SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+  ## email configs
+  MAIL_SERVER ='smtp.gmail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+  #pass
 
 class ProdConfig(Config):
     """
@@ -8,24 +20,18 @@ class ProdConfig(Config):
    Args:
        Config: The parent configuration class with General configuration settings
    """
-  pass
+  #pass
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestConfig(Config):
-    """
-  Testing configuration child class
-  Args:
-      Config: The parent configuration class with General configuration settings
-  """
-  pass
+  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+  #pass
 
 class DevConfig(Config):
-  DEBUG= True
-  pass
-
-
-
-
-
+  DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  DEBUG = True
+  #pass
 
 
 
