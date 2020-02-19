05fa8bab99ab6527008980612c0100cddc2926a2
Trinity John <trinityrtime@gmail.com>
2020-02-19 09:12:03 +0300

    views and init modification

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-#from config import config_options
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_name):
   app = Flask(__name__)
 
   #app configs
- # app.config.from_object(config_options[config_name])
+  app.config.from_object(config_options[config_name])
   #initializing flask extensions
   bootstrap.init_app(app)
 
@@ -16,6 +20,8 @@ def create_app(config_name):
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
   
-  #will add pitch and forms
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+
 
   return app
