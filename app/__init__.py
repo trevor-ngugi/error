fa0a67bd6e422325573e8b40fc9678775da5b890
trevor ngugi <ngugitrevor@gmail.com>
2020-02-21 02:32:45 +0300

    add profile picture feature

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,14 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-# from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
   
@@ -27,7 +28,8 @@ def create_app(config_name):
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
 
-  # configure_uploads(app,photos)
+  # configure UploadSet
+  configure_uploads(app,photos)
   
   from .auth import auth as auth_blueprint
   app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
   SECRET_KEY = os.environ.get("SECRET_KEY")
   #SQLALCHEMY_TRACK_MODIFICATIONS = False
-  UPLOADED_PHOTOS_DEST = 'app/static/photos'
+  UPLOADED_PHOTOS_DEST ='app/static/photos'
 
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://trevor:elite@localhost/trinity'
   
